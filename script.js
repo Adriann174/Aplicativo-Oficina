7c7ce7120c2f04f64116d6a1634342c993097a6d
Adriann <adrianalvesbrito5@gmail.com>
2025-11-19 12:33:46 -0300

    commit da aplicação

--- script.js
+++
@@ -1,9 +0,0 @@
-// Script básico para o ícone de configuração
-document.addEventListener('DOMContentLoaded', function() {
-    const configIcon = document.querySelector('.config-icon');
-    
-    configIcon.addEventListener('click', function() {
-        console.log('Ícone de configuração clicado');
-        // Aqui você pode adicionar funcionalidade futura se necessário
-    });
-});
